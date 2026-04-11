f522b1637fd8752ae09c4e4241ef1dbacd6d3f79
Muskan Gupta <muskangupta2118@gmail.com>
2026-04-11 20:06:56 +0530

    categories(Set)

---
+++ problem_16.py
@@ -0,0 +1,12 @@
+categories_set={"friut","books","pen","color"}
+
+categories_set.add("veggies")
+categories_set.add("fruit")
+print(categories_set)
+3.
+check_categories="veggies"
+exist=check_categories in categories_set
+print(exist)
+#optional
+len_categories=len(categories_set)
+print(len_categories)
