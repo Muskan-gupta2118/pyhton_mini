eb54f1319bd59d8d1f18ef08554b71475bc95ce7
Muskan Gupta <muskangupta2118@gmail.com>
2026-04-16 21:53:01 +0530

    small module for biling and discount

---
+++ problem_39.py
@@ -0,0 +1,8 @@
+def calculate_total(prices):
+    return sum(prices)
+def apply_tax(amount):
+    return amount*0.05
+def apply_discount(price,percent):
+    return price-(price*percent)
+def flat_discount(price):
+    return price-50    
