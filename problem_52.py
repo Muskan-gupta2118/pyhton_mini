39246a94e2f0826c1b52aea38a090c0106071c94
Muskan Gupta <muskangupta2118@gmail.com>
2026-04-20 21:17:51 +0530

    class and object

---
+++ problem_52.py
@@ -0,0 +1,11 @@
+#class and object
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print(self.name, self.marks)
+
+s1 = Student("Aman", 90)
+s1.display()
