8c81d08b7124b4cd5f99d2efd93e792291e069e9
Muskan Gupta <muskangupta2118@gmail.com>
2026-04-21 21:51:43 +0530

    abstract class

---
+++ problem_59.py
@@ -0,0 +1,21 @@
+#abstract problem
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+
+r = Rectangle(5, 3)
+print(r.area())
