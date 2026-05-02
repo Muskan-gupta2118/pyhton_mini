78fe1748758615afe34a08bd88c5fe5fc1bf8ccd
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-02 22:15:23 +0530

    simple bank account

---
+++ problem_98.py
@@ -0,0 +1,15 @@
+#simple bank account
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance   # private
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+
+acc = BankAccount(1000)
+acc.deposit(500)
+print(acc.get_balance())   # 1500
