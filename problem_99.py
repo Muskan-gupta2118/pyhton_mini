8d4a6d1dae0bd60a887b9c66316adfbc0396c9e8
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-02 22:17:07 +0530

    method overriding

---
+++ problem_99.py
@@ -0,0 +1,21 @@
+#Method overridinh
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
+
+d = Dog()
+c = Cat()
+
+d.sound()
+c.sound()
