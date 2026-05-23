63694ffe931ac345f5f42e16d9040ff3c09e3a32
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-23 22:25:34 +0530

    find index of row having maximum sum

--- problem_113.py
+++ problem_113.py
@@ -2,7 +2,7 @@
 import numpy as np
 arr = np.array([[1, 2, 3],
                 [10, 2, 1],
-                [4, 5, 6]])
+                [5, 5, 6]])
 
 row_sum = arr.sum(axis=1)
 index = np.argmax(row_sum)
