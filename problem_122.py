d0ae9cbc2ca8c5db9dfee29c697acf71f795882a
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-22 22:44:51 +0530

    merge dataframes

---
+++ problem_122.py
@@ -0,0 +1,22 @@
+#merge dataframes
+
+import pandas as pd
+import numpy as np
+
+students = pd.DataFrame({
+    "ID":[1,2,3],
+    "Name":["A","B","C"]
+})
+
+marks = pd.DataFrame({
+    "ID":[1,2,3],
+    "Marks":[80,90,85]
+})
+
+result = pd.merge(
+    students,
+    marks,
+    on="ID"
+)
+
+print(result)
