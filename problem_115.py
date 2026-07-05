4993160a284a5865757e5d5e6443a71674cd912f
Muskan Gupta <muskangupta2118@gmail.com>
2026-07-05 23:16:26 +0530

    adding matrix and vector

--- problem_115.py
+++ problem_115.py
@@ -3,7 +3,7 @@ import numpy as np
 matrix = np.array([[1, 2, 9],
                    [4, 5, 7]])
 
-vector = np.array([10, 20, 30])
+vector = np.array([40, 20, 30])
 
 result = matrix + vector
 print(result)
