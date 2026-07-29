9539eff65a2b581412d08ba0dc4e6af5c81434bc
Muskan gupta <muskangupta2118@gmail.com>
2026-07-29 23:05:45 +0530

    small bank account

--- problem_98.py
+++ problem_98.py
@@ -1,7 +1,7 @@
 #simple bank account
 class BankAccount:
     def __init__(self, balance):
-        self.__balance = balance   # private
+        self.__balance = balance   # automatically private
 
     def deposit(self, amount):
         self.__balance += amount
